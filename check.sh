f2a8bbc4cc1379558627f6ba148a924576c11e94
Thomas Shrader <64pigpig2014@gmail.com>
2025-12-27 14:13:56 -0500

    style: cleaner check.sh

--- check.sh
+++ check.sh
@@ -1,25 +1,25 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 OUT=out
 ALL_LANGS=0
 VALID_LANGS=0
 
 check() {
-	local LANG="$1"
-	((ALL_LANGS++))
+    local LANG="$1"
+    ((ALL_LANGS++))
 
-	VAILD=0
-	diff "out/$LANG.txt" good.txt -q > /dev/null && VAILD=1 || echo "$LANG invalid"
-
-	if (( VAILD == 1)); then
-		echo "$LANG valid"
-		((VALID_LANGS++))
-	fi
+    if diff "$OUT/$LANG.txt" good.txt -q > /dev/null; then
+        echo "$LANG valid"
+        ((VALID_LANGS++))
+    else
+        echo "$LANG invalid"
+    fi
 }
 
-while IFS= read -r line; do
-    check "$line"
-done < <(find out -maxdepth 1 -type f -exec basename {} .txt \;)
+for file in "$OUT"/*.txt; do
+    [[ -e "$file" ]] || continue
+    check "$(basename "$file" .txt)"
+done
 
 echo
-echo "$VALID_LANGS/$ALL_LANGS languages are vaild"
+echo "$VALID_LANGS/$ALL_LANGS languages are valid"
